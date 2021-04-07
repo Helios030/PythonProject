b4c5b7770dcf05fb81323fbe021082613ffe67ae
BenNiu <tunnello@163.com>
2021-04-07 23:15:35 +0800

    Initial commit

---
+++ DownloadBing.py
@@ -0,0 +1,43 @@
+import requests
+import time
+from lxml import etree
+
+import DownloadUtil
+
+# 必应图片网页地址
+url = 'https://bing.ioliu.cn/?p='
+# 浏览器参数
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+    'Referer': 'http://bing.ioliu.cn'
+}
+
+imageSize=0
+
+
+def downloadImg(index):
+
+    html = requests.get(url + str(index), headers=header).text  # 下载网页
+    etree_html = etree.HTML(html)  # 构造xpath的解析对象
+    img_url = etree_html.xpath('//img/@src')  # 获取图片地址
+    # print(img_url)
+
+    for img_list in img_url:  # 下载图片并保存至指定位置
+        global imageSize
+        imageSize=imageSize+1
+        DownloadUtil.download_from_url(img_list, "D:/PythonTest/" + str(time.time()) + ".jpg")
+
+
+
+startStr=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+start=time.time()
+for i in range(1, 10):
+    time.sleep(0.5)
+    downloadImg(i)
+
+endStr=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+chazhi=time.time()-start
+
+print("下载结束 总下载数量{},开始时间{},结束时间{} , 总用时：{}".format(str(imageSize),startStr,endStr,str(chazhi)))
+
+
